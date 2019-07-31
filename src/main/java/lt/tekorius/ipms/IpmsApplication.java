cbb050e90e4dddfbc75b4072bbe7dfe5829274b8
XFactor <tekorius.mantas@gmail.com>
2019-07-31 14:07:58 +0300

    Init commit

---
+++ src/main/java/lt/tekorius/ipms/IpmsApplication.java
@@ -0,0 +1,13 @@
+package lt.tekorius.ipms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApxplication
+public class IpmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpmsApplication.class, args);
+	}
+
+}
