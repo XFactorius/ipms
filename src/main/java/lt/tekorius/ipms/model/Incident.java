1ede4df9ef09bb9f201bc7a5483de68717304873
Mantas <tekorius.mantas@gmail.com>
2019-08-01 18:04:04 +0300

    Added unit test for IncidentService

--- src/main/java/lt/tekorius/ipms/model/Incident.java
+++ src/main/java/lt/tekorius/ipms/model/Incident.java
@@ -1,10 +1,12 @@
 package lt.tekorius.ipms.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
+@AllArgsConstructor
 public class Incident {
     private String name;
     private LocalDate startDate;
